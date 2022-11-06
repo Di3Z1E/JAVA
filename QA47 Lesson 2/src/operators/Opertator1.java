0d053fa26ef26090dc671e5c9c06068eefb49285
David Azani <davidazani@gmail.com>
2022-11-06 14:04:32 +0200

    updated

--- QA47 Lesson 2/src/operators/Opertator1.java
+++ QA47 Lesson 2/src/operators/Opertator1.java
@@ -3,7 +3,7 @@ package operators;
 public class Opertator1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		long num = 0;
 		num = 20 + 30;
 		System.out.println(num);
